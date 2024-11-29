3158c9af4bb74463964938f9353b4a219cc98775
tzhou <gmtimothy1213@gmail.com>
2024-11-29 07:31:12 -0500

    All files added

---
+++ src/components/ReactLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ReactLogo = () => {
+    return (
+        <div>ReactLogo</div>
+    )
+}
+export default ReactLogo
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,50 @@
+import React, {Suspense}  from 'react'
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+
+const Hero = () => {
+
+    const isSmall = useMediaQuery({maxWidth: 440});
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Timothy<span className="waving-hand">👋</span></p>
+                <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                {/*<Leva />*/}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
+
+                        <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
+                        <HackerRoom
+                        position={sizes.deskPosition}
+                        rotation={[0.2, 0.0, 0.0]}
+                        scale={sizes.deskScale}
+                    />
+                        <group>
+                            <Target position={[sizes.targetPosition]}/>
+                            <ReactLogo position={[sizes.reactLogoPosition]}/>
+
+                        </group>
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    )
+}
+export default Hero
